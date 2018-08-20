f371dd863498937c5079131a950e6f627b0679f5
raj.kr79@gmail.com <rajeshradhakrishnan@Rajeshs-MacBook-Pro.local>
2018-08-20 15:01:47 -0700

    updating aws scripts

---
+++ aws/aws-get-all-region-instances.py
@@ -0,0 +1,14 @@
+import boto3
+
+
+client = boto3.client('ec2', region_name='us-east-1')
+
+ec2_regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+
+for region in ec2_regions:
+    conn = boto3.resource('ec2',
+                   region_name=region)
+    instances = conn.instances.filter()
+    for instance in instances:
+#        if instance.state["Name"] == "running":
+            print (instance.id, instance.state["Name"], instance.instance_type, region)
---
+++ aws/aws-stop-all-region-instances.py
@@ -0,0 +1,24 @@
+import boto3
+import boto3.ec2
+import boto
+from boto.ec2.connection import EC2Connection
+
+client = boto3.client('ec2', region_name='us-east-1')
+
+ec2_regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+
+
+for region in ec2_regions:
+    conn = boto3.resource('ec2',
+                   region_name=region)
+    instances = conn.instances.filter()
+    for instance in instances:
+         instanceList = []
+         ec2 = boto3.resource( 'ec2', region_name=region)
+         instanceList.append(instance.id)
+         #print (instanceList)
+#        if instance.state["Name"] == "running":
+              status = ec2.instances.filter(InstanceIds=instanceList).stop()
+         #print (instance.id, status, instance.instance_type, region)
+
+import aws-get-all-region-instances.py
