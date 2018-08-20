8fb2c54e7ebea5b3ace89ea10701481a1a23b3ed
rajeshkr2016 <raj.kr9@gmail.com>
2018-08-20 07:27:53 +0100

    aws scripts

---
+++ aws/createinstances.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+    ImageId='ami-08fb94221d851e476',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro')
+print (instance[0].id)
