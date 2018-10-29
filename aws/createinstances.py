8493afb7cf2d7eaf2396f71e81945fd2738031b5
Rajesh <raj.kr79@gmail.com>
2018-10-28 22:03:00 -0700

    Updating more scipts

--- aws/createinstances.py
+++ aws/createinstances.py
@@ -2,7 +2,7 @@
 import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
-    ImageId='ami-08fb94221d851e476',
+    #ImageId='ami-08fb94221d851e476',
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro')
---
+++ aws/list_of_rds_instances.py
@@ -0,0 +1,12 @@
+import boto3
+
+rds = boto3.client('rds')
+dbs = rds.describe_db_instances()
+for db in dbs['DBInstances']:
+    print(db)
+    print (
+    db['MasterUsername'],
+    db['Endpoint']['Address'],
+    db['Endpoint']['Port'],
+    db['DBInstanceStatus']
+    )
