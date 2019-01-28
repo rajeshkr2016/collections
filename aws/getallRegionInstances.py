58c53bc98b5dd3a588f139b7e1516849dd1513e0
rradhakrishnan7 <rajesh_radhakrishnan@apple.com>
2019-01-27 22:17:11 -0800

    updating

--- aws/getallRegionInstances.py
+++ aws/getallRegionInstances.py
@@ -18,21 +18,25 @@ for region in regions:
     instances = {}
     dbs = {}
 
-    ec2_instances = ec2client.describe_instances(Filters=[ { 'Name': 'instance-state-name', 'Values': [ 'running' ] } ])
-    #ec2_instances = ec2client.describe_instances()
-    #print(ec2_instances)
+    #ec2_instances = ec2client.describe_instances(Filters=[ { 'Name': 'instance-state-name', 'Values': [ 'running' ] } ])
+    ec2_instances = ec2client.describe_instances()
+    print(ec2_instances)
     for reservation in ec2_instances['Reservations']:
         for instance in reservation['Instances']:
             instance_name =	instance['InstanceId']
+            PrivateIpAddress = instance['PrivateIpAddress']
         if 'Tags' in dict.keys(instance):
             for tag in instance['Tags']:
                 if tag['Key'] == 'Name':
                     instance_name =	instance['InstanceId'] + ' (' +	tag['Value'] + ')'
         if instance['VpcId'] in instances:
                 instances[instance['VpcId']].append(instance_name)
+                #instances[instance['VpcId']].append(ip)
         else:
-                instances[instance['VpcId']] = [ instance_name ]
-
+                inst_name = []
+                inst_name.append(instance_name)
+                instances[instance['VpcId']] = [instance_name]
+                instances[instance['VpcId']].append(PrivateIpAddress )
     db_instances = rdsclient.describe_db_instances()
     #print(db_instances)
     for db_instance in db_instances['DBInstances']:
@@ -54,8 +58,8 @@ for region in regions:
             for tag in vpc['Tags']:
                 if tag['Key'] == "Name":
                     vpc_name = tag['Value']
-            print('\033[1;32;40m' + vpc_id + ' | ' + vpc_name + ' | ' + vpc['CidrBlock'] + ' (' + count(instances, vpc_id)  + ' ec2 instances, ' + count(dbs, vpc_id) + ' rds instances)\033[0;37;40m')
+        print('\033[1;32;40m' + vpc_id + ' | ' + ' | ' + vpc['CidrBlock'] + ' (' + str(len(instances))  + ' ec2 instances, ' + str(len(dbs)) + ' rds instances)\033[0;37;40m')
         if vpc_id in instances:
-            print('\033[1;33;40m    ec2 instances: \033[0;37;40m' + ','.join(instances[vpc_id])+ '\033[1;35;40m VPC ID: \033[0;37;40m' + vpc_id)
+            print('\033[1;33;40m    ec2 instances: \033[0;37;40m' + ','.join(instances[vpc_id])+ '\033[1;35;40m IP: \033[0;37;40m' )
         if vpc_id in dbs:
-            print('\033[1;35;40m    rds instances: \033[0;37;40m' + ','.join(dbs[vpc_id]) + '\033[1;35;40m VPC ID: \033[0;37;40m' + vpc_id)
+            print('\033[1;35;40m    rds instances: \033[0;37;40m' + ','.join(dbs[vpc_id]) + '\033[1;35;40m IP: \033[0;37;40m')
